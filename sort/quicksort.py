65c076e0f445961786868ce187955ee2ec3c0896
linitachi <linitachi@gmail.com>
2020-11-07 01:02:02 +0800

    modify quicksort

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -8,13 +8,14 @@ def quick_sort(arr):
     end = len(arr) - 1
     pivot = arr[end]
     change_index = end-1
-    # 從後面來看，交換的位置在倒數第二個，如果碰到比pivot小的數字就不用交換，遇到大的，
+    # 從後面來看，交換的位置在pivot前一個，如果碰到比pivot小的數字就不用交換，遇到大的，
     # 就跟change_index的元素做交換，並且change_index往前移動一格
+    # 因為change_index在pivot前一個，所以可以保證交換後，pivot一定會比它小。
     for i in range(change_index, -1, -1):
         if arr[i] > pivot:
             arr[i], arr[change_index] = aswap(arr[i], arr[change_index])
             change_index -= 1
-    # 最後再把pivot移到change_index後面一格即可分成大小兩塊。
+    # 最後在把pivot移到change_index後面一格即可分成大小兩塊。
     arr[end], arr[change_index+1] = aswap(arr[end], arr[change_index+1])
 
     if len(arr) != 1:
