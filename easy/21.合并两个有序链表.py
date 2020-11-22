e31221c04aefa633dc2c0a6c232ada3b86f59acb
linitachi <linitachi@gmail.com>
2020-11-22 10:12:53 +0800

    update easy 21 with recursion

--- easy/21.合并两个有序链表.py
+++ easy/21.合并两个有序链表.py
@@ -33,29 +33,15 @@
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1 and not l2:
-            return None
-        elif l1 == None:
+        if l1 is None:
             return l2
-        elif l2 == None:
+        if l2 is None:
             return l1
         if l1.val < l2.val:
-            answer = l1
-            l1 = l1.next
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
         else:
-            answer = l2
-            l2 = l2.next
-
-        temp = answer
-        while l1 and l2:
-            if l1.val < l2.val:
-                temp.next = l1
-                l1 = l1.next
-            else:
-                temp.next = l2
-                l2 = l2.next
-            temp = temp.next
-        temp.next = l1 if l1 is not None else l2
-        return answer
+            l2.next = self.mergeTwoLists(l2.next, l1)
+            return l2
 
     # @lc code=end
